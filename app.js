9dddaedc4d2d6c24806ea9932183a71131c059eb
Igor K. Shiohara <ishiohara@gmail.com>
2017-02-23 22:54:10 -0300

    Creating the first route

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ var app = express();
 
 var port  = process.env.PORT || 3000;
 
+var router = express.Router();
+
+router.route('/posts')
+    .get(function (req, res) {
+        var responseJson = {hello: "My API"};
+
+        res.json(responseJson);
+    });
+
+
+app.use('/api', router);
+
 app.get('/', function (req, res) {
    res.send('Welcome to my API');
 });
